9873325e7616806f4978babe9049572affb975d9
arthyvelli <48116111+arthyvelli@users.noreply.github.com>
2021-01-09 19:34:02 +0530

    Update DriverFactory.java
    updated

--- src/main/java/core/DriverFactory.java
+++ src/main/java/core/DriverFactory.java
@@ -16,7 +16,7 @@ public class DriverFactory {
 	public WebDriver getDriver(String browserName) throws Exception {
 		switch(browserName.toLowerCase()) {  
 		  case "chrome" : 
-				 WebDriverManager.chromedriver().version("77.0.3865.40").setup();
+				 WebDriverManager.chromedriver().setup();
 				//System.setProperty("webdriver.chrome.driver",System.getProperty("user.dir")+"//drivers//chromedriver");
 			    //System.setProperty("webdriver.chrome.driver","F:\\Mailer Cloud\\Automation\\TestFramework17\\chromedriver.exe");
 		                driver =  new ChromeDriver();
